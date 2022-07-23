1847e06ccbc73ff2b6413ea803255542500ea573
Iandra <iandra.l.m.a@gmail.com>
2022-07-23 15:57:23 -0300

    exercicio cap 1

---
+++ ch1/phraseomatic.py
@@ -0,0 +1,10 @@
+import random 
+verbs = [ ' Leverage ' , 'Sync' , ' Target ' , 'Gamify' , 'Offline' , 'Crowd-sourced', '24/7' , 'Lean-in' , '30,000 foot']
+adjectives = [ 'A/B Tested', 'Freemium', 'Hyperlocal', ' Siloed' , 'B-to-B', 'Oriented', 'Cloud-based', 'API-based']
+nouns = ['Early Adopter', 'Low-hanging Fruit', ' Pipeline', 'Splash Page', 'Productivity', 'Process', 'Tipping Point', 'Paradigm' ]
+
+verb = random.choice (verbs)
+adjectives = random.choice (adjectives)
+noun = random.choice (nouns)
+phrase = verb + ' ' + adjectives + ' ' + noun 
+print (phrase)
--- ch1/sorteio.py
+++ ch1/sorteio.py
@@ -2,6 +2,17 @@ import random
 
 
 clientes = ['Jimmy', 'Kim', 'John', 'Stacie']
+sabor = ' baunilha '
+pedido = ' sundae de ' + sabor 
 vencedor = random.choice(clientes)
 
 print (' parabéns '+ vencedor +' você ganhou um sundae!')
+prompt = 'Você quer uma cereja em cima? '
+quer_cereja = input (prompt)
+if ( quer_cereja == 'sim'):
+    pedido = pedido + ' com uma cereja em cima'
+prompt = 'Você quer banana em cima ? '
+quer_banana = input (prompt)
+if (quer_banana == 'sim'): 
+    pedido = pedido + ' e uma banana em cima '
+print (' Um ' + pedido + ' para ' + vencedor + ' saindo...')
