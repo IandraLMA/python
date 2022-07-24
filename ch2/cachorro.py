4df1aeaf641c91a2f3278f31a8101594d7cc338b
Iandra <iandra.l.m.a@gmail.com>
2022-07-24 19:49:43 -0300

    exercicio idade cachorro

---
+++ ch2/cachorro.py
@@ -0,0 +1,10 @@
+dog_name = input ( ' What is your dog`s name? 1')
+print('sdsdds')
+dog_age = input ( ' What is your dog`s age? ')
+dog_age = int(dog_age) *7
+print('O nome do seu cachorro é '+ dog_name)
+print('A idade do seu cachorro é '+ str(dog_age))
+
+
+
+
