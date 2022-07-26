afa42db8627ab15043baa9f9a7f8c6b19b6dba02
Iandra <iandra.l.m.a@gmail.com>
2022-07-26 16:54:03 -0300

    teste

--- ch2/las.py
+++ ch2/las.py
@@ -2,6 +2,8 @@ first = 'somewhere'
 last = 'over the rainbow'
 full = first + " " + last 
 last = last + " bola "
+name = "Rodrigo"
+namorada = "Iandra"
 print (first, last)
 print ( first + " " + last)
 print ( full )
