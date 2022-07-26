eaf525bec49bbe8cc66cf4dbe582e4d283c6400d
Iandra <iandra.l.m.a@gmail.com>
2022-07-26 16:49:06 -0300

    capitulo 2 finalizado

--- ch2/cachorro.py
+++ ch2/cachorro.py
@@ -1,9 +1,9 @@
-dog_name = input ( ' What is your dog`s name? 1')
-print('sdsdds')
+dog_name = input ( ' What is your dog`s name? ')
 dog_age = input ( ' What is your dog`s age? ')
 dog_age = int(dog_age) *7
 print('O nome do seu cachorro é '+ dog_name)
 print('A idade do seu cachorro é '+ str(dog_age))
+print (' Your dog ', dog_name  , ' is ', dog_age , ' years old in human years' )
 
 
 
---
+++ ch2/las.py
@@ -0,0 +1,8 @@
+first = 'somewhere'
+last = 'over the rainbow'
+full = first + " " + last 
+last = last + " bola "
+print (first, last)
+print ( first + " " + last)
+print ( full )
+
