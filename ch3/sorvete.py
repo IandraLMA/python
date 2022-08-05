f3c5be6ff5a45b4071ad68f56d76eb3400c3e563
Iandra <iandra.l.m.a@gmail.com>
2022-08-05 18:27:20 -0300

    atividade

---
+++ ch3/sorvete.py
@@ -0,0 +1,8 @@
+scoops = 5
+while scoops >0:
+    print ("Another scoops!")
+    scoops=scoops -1
+    if scoops == 3:
+        print ("O sorvete esta na segunda bola")
+    print ("Life without ice cream isn`t the same")           
+print ("Life without ice cream isn`t the same")
